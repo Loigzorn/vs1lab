4c429834118e68c6735150fbe46be0629130c43e
JulianBucher <Measterjb@gmail.com>
2022-05-28 19:36:42 +0200

    Feature: Add task 03

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -4,40 +4,47 @@
 
 // This script is executed when the browser loads index.html.
 
-// "console.log" writes to the browser's console. 
+// "console.log" writes to the browser's console.
 // The console window must be opened explicitly in the browser.
 // Try to find this output in the browser...
 console.log("The geoTagging script is going to start...");
-
-
-
-
-
 
 /**
  * A function to retrieve the current location and update the page.
  * It is called once the page has been fully loaded.
  */
 function updateLocation(helper) {
-    const map = new MapManager("bWQM84jzA43ETIOGOIyfighZXKAUFXmm");
+  const map = new MapManager("bWQM84jzA43ETIOGOIyfighZXKAUFXmm");
 
-    const latitude = helper.latitude;
-    const longitude = helper.longitude;
+  const latitude = helper.latitude;
+  const longitude = helper.longitude;
+  const tags = helper.tags;
 
-    const mapURL = map.getMapUrl(latitude, longitude)
+  const mapURL = map.getMapUrl(latitude, longitude, tags);
 
-    document.getElementById('latitude').attributes[3].nodeValue = latitude;
-    document.getElementById('longitude').attributes[3].nodeValue = longitude;
+  document.getElementById("latitude").attributes[3].nodeValue = latitude;
+  document.getElementById("longitude").attributes[3].nodeValue = longitude;
 
-    document.getElementById('mapView').attributes.getNamedItem("src").value = mapURL;
-    document.getElementById('searchLatitude').value = latitude;
-    document.getElementById('searchLongitude').value = longitude;
+  document.getElementById("mapView").attributes.getNamedItem("src").value =
+    mapURL;
+  //NOTE Added data-tags call
+  document
+    .getElementById("mapView")
+    .attributes.getNamedItem("data-tags").value = JSON.stringify(
+    mapURL.tagList
+  );
+
+  document.getElementById("searchLatitude").value = latitude;
+  document.getElementById("searchLongitude").value = longitude;
+  document.getElementById("searchTag").value = tags; //NOTE added searchTag 
 }
-
 
 // Wait for the page to fully load its DOM content, then call updateLocation
 document.addEventListener("DOMContentLoaded", () => {
-    if ((document.getElementById('searchLongitude').value=='')&&(document.getElementById('searchLatitude').value=='')){
+  if (
+    document.getElementById("searchLongitude").value == "" &&
+    document.getElementById("searchLatitude").value == ""
+  ) {
     LocationHelper.findLocation(updateLocation);
-    }
+  }
 });
