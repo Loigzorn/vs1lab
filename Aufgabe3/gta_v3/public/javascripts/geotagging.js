3ace31b022771d6f4ee4d003da91ab7edc2c22c6
Loigzorn <loigzorn@pm.me>
2022-05-30 01:41:27 +0200

    [Feature] Write data back to DOM

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -14,29 +14,29 @@ console.log("The geoTagging script is going to start...");
  * It is called once the page has been fully loaded.
  */
 function updateLocation(helper) {
-  const map = new MapManager("bWQM84jzA43ETIOGOIyfighZXKAUFXmm");
 
   const latitude = helper.latitude;
   const longitude = helper.longitude;
-  const tags = helper.tags;
 
-  const mapURL = map.getMapUrl(latitude, longitude, tags);
-
-  document.getElementById("latitude").attributes[3].nodeValue = latitude;
-  document.getElementById("longitude").attributes[3].nodeValue = longitude;
-
-  document.getElementById("mapView").attributes.getNamedItem("src").value = mapURL;
+  document.getElementById("latitude").value = latitude;
+  document.getElementById("longitude").value = longitude;
 
   document.getElementById("searchLatitude").value = latitude;
   document.getElementById("searchLongitude").value = longitude;
-  var mapViewImage = document.getElementById("mapView");
-  mapViewImage.attributes.getNamedItem("data-tags").value = JSON.stringify(null);
-  console.log(mapViewImage);
+
+  let imageView = document.getElementById("mapView");
+  let tagsAsString = imageView.dataset.tags
+  let tags = JSON.parse(tagsAsString);
+
+  const map = new MapManager("bWQM84jzA43ETIOGOIyfighZXKAUFXmm");
+  const mapURL = map.getMapUrl(latitude, longitude, tags);
+  document.getElementById("mapView").attributes.getNamedItem("src").value = mapURL;
+
 }
 
 // Wait for the page to fully load its DOM content, then call updateLocation
 // It is senseless to wait for the DOMContentLoaded event, 
 // as the event has already happend at this stage.
-if (document.getElementById("searchLongitude").value == "" && document.getElementById("searchLatitude").value == "") {
+if (JSON.stringify(document.getElementById("searchLongitude").value).match("\d")) {
   LocationHelper.findLocation(updateLocation);
 }
