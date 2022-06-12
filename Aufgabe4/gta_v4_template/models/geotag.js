b932bc4005f719ff572c8f4b7c8f2fc025e1ca72
JulianBucher <Measterjb@gmail.com>
2022-06-12 22:27:03 +0200

    geotag.js: Geotags mit uuid (URI) erweitert

--- Aufgabe4/gta_v4_template/models/geotag.js
+++ Aufgabe4/gta_v4_template/models/geotag.js
@@ -5,47 +5,55 @@
  * Complete all TODOs in the code documentation.
  */
 
-/** * 
- * A class representing geotags.
- * GeoTag objects should contain at least all fields of the tagging form.
- */
-class GeoTag {
+ const {v4: uuidv4} = require("uuid");
 
-    latitude;
-    longitude;
-    tagName = '';
-    hashtag = '';
-
-    /**
-    * Create a new GeoTag instance.
-    * @param {int} latitude The latitude of the geoTag
-    * @param {int} longitude The longitude of the geoTag
-    * @param {string} tagName The tag name of the geoTag
-    * @param {string} hashtag The hashtag of the geoTag
-    */
-    constructor(latitude, longitude, tagName, hashtag) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.tagName = tagName;
-        this.hashtag = hashtag;
-        return this;
-    }
-
-    get latitude() {
-        return this.latitude;
-    }
-
-    get longitude() {
-        return this.longitude;
-    }
-
-    get tagName() {
-        return this.tagName;
-    }
-
-    get hashtag() {
-        return this.hashtag;
-    }
-}
-
-module.exports = GeoTag;
+ /** *
+  * A class representing geotags.
+  * GeoTag objects should contain at least all fields of the tagging form.
+  */
+ class GeoTag {
+ 
+     uuid; //The Universally Unique Identifier of the geoTag
+     latitude;
+     longitude;
+     tagName = '';
+     hashtag = '';
+ 
+     /**
+      * Create a new GeoTag instance.
+      * @param {int} latitude The latitude of the geoTag
+      * @param {int} longitude The longitude of the geoTag
+      * @param {string} tagName The tag name of the geoTag
+      * @param {string} hashtag The hashtag of the geoTag
+      */
+     constructor( latitude, longitude, tagName, hashtag) {
+         this.uuid = uuidv4();
+         this.latitude = latitude;
+         this.longitude = longitude;
+         this.tagName = tagName;
+         this.hashtag = hashtag;
+         return this;
+     }
+ 
+     get uuid() {
+         return this.uuid
+     }
+ 
+     get latitude() {
+         return this.latitude;
+     }
+ 
+     get longitude() {
+         return this.longitude;
+     }
+ 
+     get tagName() {
+         return this.tagName;
+     }
+ 
+     get hashtag() {
+         return this.hashtag;
+     }
+ }
+ 
+ module.exports = GeoTag;
